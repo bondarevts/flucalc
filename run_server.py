e909976b86b8c1c36be5444a454ed31f27d9ebaf
Timofey Bondarev <bondarevts@gmail.com>
2016-01-25 00:51:52 -0500

    add ip:port argument to run_server command

--- run_server.py
+++ run_server.py
@@ -4,7 +4,20 @@ from flucalc.server import app
 
 
 def main():
-    app.run(debug='--debug' in sys.argv)
+    ip = '127.0.0.1'
+    port = 5000
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            if ':' in arg:
+                ip, port = arg.split(':')
+                port = int(port)
+                break
+            if '.' in arg:
+                ip = arg
+            if arg.isdigit():
+                port = int(arg)
+    debug = '--debug' in sys.argv
+    app.run(host=ip, port=port, debug=debug)
 
 
 if __name__ == '__main__':
