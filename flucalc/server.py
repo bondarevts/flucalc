13c58ba5b0756e8f9acbfde2820ee17bac99facc
Timofey Bondarev <bondarevts@gmail.com>
2016-01-30 16:10:05 -0500

    Create own field type for multiple numbers; move error selection to function

--- flucalc/server.py
+++ flucalc/server.py
@@ -13,6 +13,8 @@ app = flask.Flask(__name__)
 app.secret_key = keys.secret_key
 
 
+FieldError = namedtuple('FieldError', ['field_name', 'message'])
+
 MediaDescription = namedtuple('MediaDescription', ['c', 'd', 'v'])
 
 
@@ -20,56 +22,86 @@ Values = namedtuple('Values', ['m', 'mu', 'mu_interval'])
 CalcResult = namedtuple('CalcResult', ['raw', 'corrected', 'mean_frequency'])
 
 
-def volume_validator(_, field):
+def volume_value(_, field):
     if field.data is not None and float(field.data) <= 0:
         raise ValidationError('Non positive volume')
 
 
-def clones_validator(_, field):
-    assert field.data
-    clones = []
-    for pos, clone in enumerate(field.data.split(), 1):
-        try:
-            value = float(clone)
-        except ValueError:
-            if len(clone) > 5:
-                clone = clone[:5] + '...'
-            message = 'Value #{} is not a valid float: "{}"'.format(pos, clone)
-            raise ValidationError(message)
-        clones.append(value)
-    if any(clone < 0 for clone in clones):
-        raise ValidationError('Negative number of clones')
+def clones_numbers(_, field):
+    if field.data is not None:
+        if any(clone < 0 for clone in field.data):
+            raise ValidationError('Negative number of clones')
+
+
+def int_values(_, field):
+    if field.data is not None:
+        if not all(value.is_integer() for value in field.data):
+            raise ValidationError('Number must be integer')
 
 
 dilution_validator = validators.NumberRange(min=1, message='Must be &#8805; 1')
 
 
+# noinspection PyAttributeOutsideInit
+class MultiFloatAreaField(TextAreaField):
+    def _value(self):
+        if self.raw_data:
+            return self.raw_data[0]
+        if self.data:
+            return '\n'.join(self.data)
+        return ''
+
+    def process_formdata(self, valuelist):
+        if not valuelist:
+            return
+        data = []
+        for pos, value in enumerate(valuelist[0].split(), 1):
+            try:
+                float_value = float(value)
+            except ValueError as e:
+                self.data = None
+                if len(value) > 5:
+                    value = value[:5] + '...'
+                message = 'Value #{} is not a valid float: "{}"'.format(pos, value)
+                raise ValueError(message) from e
+            data.append(float_value)
+        self.data = data
+
+
 class FluctuationInputForm(Form):
     v_total = FloatField('Volume of a culture <i>(&#956;l)</i>, V<sub>tot</sub>',
-                         [volume_validator], default=200)
-    c_selective = TextAreaField('Observed numbers of clones, C<sub>sel</sub>',
-                                [validators.DataRequired(), clones_validator])
+                         [volume_value], default=200)
+    c_selective = MultiFloatAreaField('Observed numbers of clones, C<sub>sel</sub>',
+                                      [clones_numbers, int_values])
     d_selective = FloatField('Dilution factor, D<sub>sel</sub>', [dilution_validator])
-    v_selective = FloatField('Volume plated <i>(&#956;l)</i>, V<sub>sel</sub>', [volume_validator])
+    v_selective = FloatField('Volume plated <i>(&#956;l)</i>, V<sub>sel</sub>', [volume_value])
 
-    c_complete = TextAreaField('Observed numbers of clones, C<sub>com</sub>',
-                               [validators.DataRequired(), clones_validator])
+    c_complete = MultiFloatAreaField('Observed numbers of clones, C<sub>com</sub>',
+                                     [clones_numbers])
     d_complete = FloatField('Dilution factor, D<sub>com</sub>', [dilution_validator])
-    v_complete = FloatField('Volume plated <i>(&#956;l)</i>, V<sub>com</sub>', [volume_validator])
+    v_complete = FloatField('Volume plated <i>(&#956;l)</i>, V<sub>com</sub>', [volume_value])
 
     submit = SubmitField("Calculate")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
-    form = FluctuationInputForm(csrf_enabled=False)
+    form = FluctuationInputForm(flask.request.form, csrf_enabled=False)
     if not form.is_submitted():
         return flask.render_template('input_form.html', form=form)
 
     if form.validate():
-        result_data = process_input(flask.request.form)
+        result_data = process_input(form)
         return flask.render_template('result.html', results=result_data)
 
+    for error in get_errors(form):
+        message = '<code>{error.field_name}:</code> {error.message}'.format(error=error)
+        flask.flash(flask.Markup(message))
+
+    return flask.render_template('input_form.html', form=form)
+
+
+def get_errors(form):
     for field in form:
         try:
             # extract field symbol
@@ -78,16 +110,21 @@ def main_page():
             field_name = field.label.text
         if field.errors:
             message = '; '.join(error.lower().rstrip('.') for error in field.errors)
-            flask.flash(flask.Markup('<code>{}:</code> {}'.format(field_name, message)))
-
-    return flask.render_template('input_form.html', form=form)
+            yield FieldError(field_name, message)
 
 
 def process_input(form):
-    v_total = float(form['v_total'])
-    selective = parse_media_description(form, section='selective')
-    complete = parse_media_description(form, section='complete')
-    complete = complete._replace(c=mean(complete.c))
+    v_total = form.v_total.data
+    selective = MediaDescription(
+        c=form.c_selective.data,
+        d=form.d_selective.data,
+        v=form.v_selective.data
+    )
+    complete = MediaDescription(
+        c=mean(form.c_complete.data),
+        d=form.d_complete.data,
+        v=form.v_complete.data
+    )
 
     c_complete_to_selective = complete.c * selective.v * complete.d / complete.v / selective.d
 
@@ -119,16 +156,3 @@ def calc_corrected_results(raw_results, selective, v_total):
 
 def calc_z(media_description, v_total):
     return media_description.v / media_description.d / v_total
-
-
-def parse_media_description(form, *, section):
-    """ Extract media description values from the form.
-    :param form: `FluctuationInputForm` object
-    :param section: ``'complete'`` or ``'selective'``
-    :return: `MediaDescription` value
-    """
-    return MediaDescription(
-            c=[float(row) for row in form['c_' + section].split()],
-            d=float(form['d_' + section]),
-            v=float(form['v_' + section])
-    )
